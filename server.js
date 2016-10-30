fc36bb76bc44cdeaff0df71fdf578fd8f1beb793
aahurtado <aaronhurtado95@gmail.com>
2016-10-29 20:20:46 -0700

    Fixed server.js

--- server.js
+++ server.js
@@ -1,42 +1,3 @@
-// /**
-//  * Introduction to Human-Computer Interaction
-//  * Lab 1
-//  * --------------
-//  * Created by: Michael Bernstein
-//  * Last updated: December 2013
-//  */
-// var PORT = 3000;
-
-// // Express is a web framework for node.js
-// // that makes nontrivial applications easier to build
-// var express = require('express');
-// var exphbs  = require('express-handlebars');
-
-// // Create the server instance
-// var app = express();
-
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-
-
-// // Print logs to the console and compress pages we send
-// app.use(express.logger());
-// app.use(express.compress());
-
-// // Return all pages in the /static directory
-// // whenever they are requested at '/'
-// // e.g., http://localhost:3000/index.html
-// // maps to /static/index.html on this machine
-// app.use(express.static(__dirname + '/static'));
-
-// // Start the server
-// var port = process.env.PORT || PORT; // 80 for web, 3000 for development
-// app.listen(port, function() {
-// 	console.log("Node.js server running on port %s", port);
-// });
-
-var PORT = 3000;
-
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
@@ -44,7 +5,6 @@ var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 app.use(express.logger());
 app.use(express.compress());
 app.use(express.static('static'));
@@ -97,8 +57,8 @@ app.all('*', function(req, res) {
   res.redirect('/');
 });
 
+var PORT = 3000;
 var port = process.env.PORT || PORT; // 80 for web, 3000 for development
 app.listen(port, function() {
   console.log("Node.js server running on port %s", port);
 });
-//app.listen(3000);
