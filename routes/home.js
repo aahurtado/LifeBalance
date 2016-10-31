4383c392c3de25a52049b91edef9bac2f0f97ce9
aahurtado <aaronhurtado95@gmail.com>
2016-10-31 14:43:49 -0700

    Trying to do add event

--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,6 @@ var data = require('../data.json');
  * GET home page.
  */
  exports.view = function(req, res){
+ 	console.log("yay,home just ran!");
  	res.render('home', data);
  };
--- static/js/home.js
+++ static/js/home.js
@@ -1,13 +1,21 @@
-function deleteEventCard() {
-	$("#event1").fadeOut();
-};
+// Represents the most recently clicked event card
+var mostRecClickedEvent;
 
 /*
+ * Defines click function for when an event card is clicked
+ */
 $(".raised.blue.card").click(function(e) {
-	$(this).fadeOut();
+	mostRecClickedEvent = $(this);
 });
-*/
+
+/*
+ * Defines click function for when the "Yes" button is clicked
+ * in the delete modal
+ */
+$("#delete_modal_yes").click(function(e) {
+	mostRecClickedEvent.fadeOut();
+});
 
 $(document).ready(function() {
-	
+
 });
