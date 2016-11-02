396f3f23abb48e9b4c5c73ce358b177230ccd993
aahurtado <aaronhurtado95@gmail.com>
2016-11-02 16:15:13 -0700

    Sign up page works

--- static/js/sign_in.js
+++ static/js/sign_in.js
@@ -1,9 +1,13 @@
 /*
  * Defines click function for when an event card is clicked
  */
-$('#sign_in_form').form({
-    fields: {
-        email: ['email', 'empty'],
-        password: ['minLength[6]', 'empty']
-    }
+$(document).ready(function() {
+
+    $('#sign_in_form').form({
+        fields: {
+            email: ['email', 'empty'],
+            password: ['minLength[6]', 'empty']
+        }
+    });
+
 });
---
+++ static/js/sign_up.js
@@ -0,0 +1,14 @@
+/*
+ * Defines click function for when an event card is clicked
+ */
+$(document).ready(function() {
+
+    $('#sign_up_form').form({
+        fields: {
+            email: ['email', 'empty'],
+            password: ['minLength[6]', 'empty'],
+            password2: ['match[password]']
+        }
+    });
+
+});
