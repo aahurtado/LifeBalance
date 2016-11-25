353776c24544fcab9e48da3a9804204089d4ffdd
Armin Vazehgoo <arminvazehgoo@Armins-MBP.attlocal.net>
2016-11-24 17:06:22 -0800

    Tracker for Time Spent on Edit Module added

--- static/js/home2.js
+++ static/js/home2.js
@@ -75,6 +75,7 @@ $("#add_modal_cancel").click(function (e) {
 $("#edit_modal_cancel").click(function (e) {
     elapsed = new Date().getTime() - start;
     alert("Time Elapsed Since Opening Edit Module in Milliseconds: " + elapsed,5000);
+    ga('send', 'timing', 'Edit Module', 'Edit Canceled', elapsed);
     $('#edit_modal_form').form('clear');
 });
 
@@ -116,6 +117,7 @@ $(document).ready(function () {
         alert("Time Elapsed Since Opening Edit Module in Milliseconds: " + elapsed,5000);
         document.getElementById('edit_modal_form_hiddenID').value = mostRecClickedEvent[0].id;
         updateGraph();
+        ga('send', 'timing', 'Edit Module', 'Edit Saved', elapsed);
     });
 
     /*
